dd4eacb3cc8f0ccc678e80e691a9ea5de62f8b2c
diazbetancur <49962109+diazbetancur@users.noreply.github.com>
2021-04-09 23:03:23 -0500

    Carga Imagenes

--- navigations/RestaurantsStack.js
+++ navigations/RestaurantsStack.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { createStackNavigator} from '@react-navigation/stack'
 
-import Restaurants from '../screens/Restaurants'
+import Restaurants from '../screens/restaurants/Restaurants'
+import AddRestaurant from '../screens/restaurants/AddRestaurant'
 
 const Stack = createStackNavigator()
 
@@ -12,7 +13,12 @@ export default function RestaurantsStack() {
                 name="restaurants"
                 component={Restaurants}
                 options={{ title: "Restaurantes"}}
-            />                
+            />   
+            <Stack.Screen
+                name="add-restaurant"
+                component={AddRestaurant}
+                options={{ title: "Crear Restaurante"}}
+            />             
         </Stack.Navigator>
     )
 }
